bfdabad0b69ce0cc8b8b0c83635dcc221fc36e64
Riguz Lee <dr@riguz.com>
2023-12-12 14:00:18 +0800

    updated encoder

--- data/shuixu_full/prepare.py
+++ data/shuixu_full/prepare.py
@@ -1,50 +1,40 @@
 import os
 import pickle
+import tiktoken
 import numpy as np
+import random
 
+# Ref: https://juejin.cn/post/7213945427853672507
 # cat *.txt > input.txt
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 
-with open(input_file_path, 'r') as f:
-    data = f.read()
-print(f"length of dataset in characters: {len(data):,}")
+entries = []
+with open(input_file_path, "r") as f:
+    for line in f:
+        if line.strip() and len(line) > 2:
+            entries.append(line)
 
-# get all the unique characters that occur in this text
-chars = sorted(list(set(data)))
-vocab_size = len(chars)
-print("all the unique characters:", ''.join(chars))
-print(f"vocab size: {vocab_size:,}")
+print(f"len of lines: {len(entries)}")
+# Shuffle entries
+random.shuffle(entries)
 
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-def encode(s):
-    return [stoi[c] for c in s] # encoder: take a string, output a list of integers
-def decode(l):
-    return ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+n = len(entries)
+train_entries = entries[: int(n * 0.9)]
+val_entries = entries[int(n * 0.9):]
 
-# create the train and test splits
-n = len(data)
-train_data = data[:int(n*0.9)]
-val_data = data[int(n*0.9):]
+# Turn those into strings
+train_data = " ".join("{}".format(entry) for entry in train_entries)
+val_data = " ".join("{}".format(entry) for entry in val_entries)
 
-# encode both to integers
-train_ids = encode(train_data)
-val_ids = encode(val_data)
+# encode with tiktoken gpt2 bpe
+enc = tiktoken.get_encoding("gpt2")
+train_ids = enc.encode_ordinary(train_data)
+val_ids = enc.encode_ordinary(val_data)
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
 
 # export to bin files
 train_ids = np.array(train_ids, dtype=np.uint16)
 val_ids = np.array(val_ids, dtype=np.uint16)
-train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
-val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
-
-# save the meta information as well, to help us encode/decode later
-meta = {
-    'vocab_size': vocab_size,
-    'itos': itos,
-    'stoi': stoi,
-}
-with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
-    pickle.dump(meta, f)
+train_ids.tofile(os.path.join(os.path.dirname(__file__), "train.bin"))
+val_ids.tofile(os.path.join(os.path.dirname(__file__), "val.bin"))
