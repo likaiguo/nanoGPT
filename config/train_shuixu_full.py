86df2d76d88ea75eb95e7ca95698d651531d01e4
Riguz Lee <dr@riguz.com>
2023-12-12 16:32:43 +0800

    updated

--- config/train_shuixu_full.py
+++ config/train_shuixu_full.py
@@ -13,7 +13,7 @@ wandb_run_name = 'mini-gpt'
 dataset = 'shuixu_full'
 gradient_accumulation_steps = 1
 batch_size = 16
-block_size = 512 # context of up to 256 previous characters
+block_size = 512
 
 # baby GPT model :)
 n_layer = 8
@@ -21,10 +21,10 @@ n_head = 8
 n_embd = 768
 dropout = 0.2
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+learning_rate = 1e-4 # with baby networks can afford to go a bit higher
 max_iters = 20000
 lr_decay_iters = 20000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
+min_lr = 1e-5 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
