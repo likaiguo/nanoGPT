98b736858f1cabacfab78eaa420cb2e9743e8136
Riguz Lee <dr@riguz.com>
2023-12-12 15:04:39 +0800

    increase params

--- config/train_shuixu_full.py
+++ config/train_shuixu_full.py
@@ -13,17 +13,17 @@ wandb_run_name = 'mini-gpt'
 dataset = 'shuixu_full'
 gradient_accumulation_steps = 1
 batch_size = 32
-block_size = 256 # context of up to 256 previous characters
+block_size = 512 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
+n_layer = 8
+n_head = 8
+n_embd = 768
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 20000
+lr_decay_iters = 20000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
